a2f6923bac2d34c81715aca728159f87122fd0fe
LiamMahoney <liammahoney96@gmail.com>
2021-03-25 22:26:38 -0500

    updated long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Liam Mahoney",
     author_email="liammahoney96@gmail.com",
     description="A library for creating asynchronous Custom Threat Services for use with the IBM Resilient SOAR platform",
-    long_description="A library for creating and running an asyncrhonous Custom Threat Service for the IBM Resilient 'Asynchronous CTS Hub' app",
+    long_description="A library for creating and running an asyncrhonous Custom Threat Service that enriches artifacts for the IBM Resilient SOAR platform.",
     url="https://github.com/LiamMahoney/resilient_async_cts",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -16,4 +16,7 @@ setuptools.setup(
     ],
     #TODO: verify
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': ['resilient_async_cts=resilient_async_cts.codegen.codegen:main']
+    }
 )
